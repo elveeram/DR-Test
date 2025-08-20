4e08881995690c3dc058000ce7c2d5a39e65e28d
Elveera Mathias <emathias@redhat.com>
2025-08-20 09:16:00 -0400

    update delete script cluster specific

--- delete_resource.go
+++ delete_resource.go
@@ -2,11 +2,30 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
 	"strings"
 )
+
+var initialListCmd string
+var initialListArgs []string
+
+type BackupStorageLocation struct {
+	Spec Spec `json:"spec"`
+}
+
+// Spec matches the nested "spec" object.
+type Spec struct {
+	ObjectStorage ObjectStorage `json:"objectStorage"`
+}
+
+// ObjectStorage matches the nested "objectStorage" object.
+type ObjectStorage struct {
+	Bucket string `json:"bucket"`
+}
 
 // runCommand captures the stdout of a given command and returns it as a slice of strings,
 // where each string is a line from the command's output.
@@ -48,116 +67,129 @@ func runCommand(command string, args ...string) ([]string, error) {
 	return firstColumnList, nil
 }
 
-// getMetadataNameFromYAML extracts the metadata.name for a given kind from a YAML file.
-func getMetadataNameFromYAML(kind, filePath string) (string, error) {
-	fmt.Printf("\n--- Extracting metadata.name for kind '%s' from '%s' ---\n", kind, filePath)
-
-	// Construct the yq command
-	yqCmd := fmt.Sprintf("yq '. | select(.kind == \"%s\") | .metadata.name' %s", kind, filePath)
-
-	// Execute the yq command
-	yqCmdOutput, err := runCommand("bash", "-c", yqCmd)
-	if err != nil {
-		return "", fmt.Errorf("failed to execute yq command: %s", err)
-	}
-
-	extractedName := strings.TrimSpace(yqCmdOutput[0])
-	if extractedName == "" {
-		return "", fmt.Errorf("no metadata.name found for kind '%s' in file '%s'", kind, filePath)
-	}
-
-	fmt.Printf("Extracted name: %s\n", extractedName)
-	fmt.Println("--- Metadata Name Extraction Completed ---")
-	return extractedName, nil
-}
-
 // deleteBackup removes items from the original list that are present in the itemsToRemove list.
 // It deletes the list items.
-func deleteResource(resourceToDelete, kind, filePath string) {
-	var initialListCmd string
-	var initialListArgs []string
+func deleteResource(resourceToDelete, clusterId string) {
 
-	extractName, err := getMetadataNameFromYAML(kind, filePath)
-	if resourceToDelete != "backup" {
-		initialListCmd = "oc"
-		initialListArgs = []string{"get", resourceToDelete, extractName}
-	} else if strings.Contains(extractName, extractName) {
-		initialListCmd = "oc"
-		initialListArgs = []string{"get", resourceToDelete}
-	} else {
-		fmt.Println("Continuing deleting openshift resources.")
-	}
-
-	fmt.Println("--- Getting Initial List ---")
-	itemsToRemove, err := runCommand(initialListCmd, initialListArgs...)
-	if err != nil {
-		//fmt.Printf("Error getting initial list: %v\n", err)
-		return
-	}
-	// Create a map for quick lookup of items to remove
-	//removeMap := make(map[string]struct{})
-	var cmdCommand string
-	var cmdArgs []string
-
-	cmdCommand = "oc"
-
-	for _, item := range itemsToRemove {
-		cmdArgs = []string{"delete", resourceToDelete, item}
+	var backupList []string
+	//var itemsToRemove []string
+	initialListCmd = "oc"
+	if resourceToDelete == "schedule" || resourceToDelete == "bsl" {
+		var cmdArgs []string
+		var cmdCommand = "oc"
+		initialListArgs = []string{"get", resourceToDelete, clusterId + "-hourly"}
+		fmt.Println("--- Getting Initial List ---", initialListArgs)
+		itemsToRemove, err := runCommand(initialListCmd, initialListArgs...)
+		if err != nil {
+			fmt.Printf("Error getting initial list: %v\n", err)
+			return
+		}
+		fmt.Printf("The entire list of items to remove: %s", itemsToRemove[1])
+		cmdArgs = []string{"delete", resourceToDelete, itemsToRemove[1]}
 		cmd := exec.Command(cmdCommand, cmdArgs...)
-		fmt.Println(cmd)
 		stdout, err := cmd.Output()
 		if err != nil {
 			fmt.Errorf("failed to get StdoutPipe: %w", err)
 		}
-		fmt.Println("All good: %w", stdout)
+		fmt.Printf("All good: %w", stdout)
+		fmt.Printf("The cmd command is: %s", cmd)
+	} else {
+		initialListArgs = []string{"get", resourceToDelete}
+		itemsToRemove, err := runCommand(initialListCmd, initialListArgs...)
+		for _, item := range itemsToRemove {
+			if strings.Contains(item, clusterId) {
+				backupList = append(backupList, item)
+			}
+		}
+		if err != nil {
+			fmt.Printf("Error getting initial list: %v\n", err)
+		}
+		fmt.Printf("The entire list of backup items to remove: %s", backupList)
+		var cmdArgs []string
+		var cmdCommand = "oc"
+		for _, item := range backupList {
+			cmdArgs = []string{"delete", resourceToDelete, item}
+			cmd := exec.Command(cmdCommand, cmdArgs...)
+			fmt.Println(cmd)
+			stdout, err := cmd.Output()
+			if err != nil {
+				fmt.Errorf("failed to get StdoutPipe: %w", err)
+			}
+			log.Println(string(stdout))
+			fmt.Printf("Resource deleted: %w", item)
 
+		}
 	}
 }
 
 // cleanupAWSResources performs a series of AWS cleanup operations.
 // It takes the IAM role name, S3 bucket name, and KMS key ARN as input.
-func cleanupAWSResources(roleName, bucketName, clusterId, mcName string) error {
+func cleanupAWSResources(clusterId, mcName string) error {
+	// --- Get S3 bucket name ---
+	initialListCmd = "oc"
+	initialListArgs = []string{"get", "bsl", clusterId + "-hourly", "-o", "json"}
+
+	cmd := exec.Command(initialListCmd, initialListArgs...)
+	fmt.Println(initialListArgs)
+	bucketNameOut, err := cmd.Output()
+	if err != nil {
+		fmt.Errorf("failed to get StdoutPipe: %w", err)
+	}
+
+	fmt.Println("S3 bucket name JSON is ", string(bucketNameOut))
+	var bsl BackupStorageLocation
+
+	// Unmarshal (parse) the JSON string into the struct.
+	s3bucketerr := json.Unmarshal([]byte(bucketNameOut), &bsl)
+	if s3bucketerr != nil {
+		fmt.Println("Error parsing JSON:", err)
+		//return
+	}
+
+	// Access the nested bucket value and print it.
+	bucketName := bsl.Spec.ObjectStorage.Bucket
+	fmt.Println(bucketName)
 
 	// --- IAM Operations ---
-
+	var roleNamePrefix = "rosa-hcp-bkp-"
 	// List all policies in the role
-	rolePolicyListCmd := "aws iam list-attached-role-policies --role-name rosa-hcp-bkp-" + mcName + "-" + clusterId + " | awk '{print $2}'"
+	awsCmd := "aws"
+	rolePolicyListArgs := []string{"iam", "list-attached-role-policies", "--role-name", roleNamePrefix + mcName + "-" + clusterId, "|", "awk", "{print $2}"}
 
-	rolePolicyListOutput, err := runCommand("bash", "-c", rolePolicyListCmd)
+	rolePolicyListOutput, err := runCommand(awsCmd, rolePolicyListArgs...)
 	if err != nil {
-		return fmt.Errorf("Policies are empty or role does not exist: %s", err)
+		fmt.Printf("Policies are empty or role does not exist: %s", err)
 	}
-	fmt.Printf("Role policies list is as follows...\n")
+	fmt.Printf("Role policies list Output is as follows... %s\n", rolePolicyListOutput)
 
-	var detachIAMRolePolicyCmd, deleteRoleCmd string
 	for _, item := range rolePolicyListOutput {
 		// 1. Detach IAM Role Policy
-		detachIAMRolePolicyCmd = "aws iam detach-role-policy --policy-arn " + item + " --role-name rosa-hcp-bkp-" + mcName + "-" + clusterId
+		var detachIAMRolePolicyListArgs = []string{"iam", "detach-role-policy", "--policy-arn", item, "--role-name", "rosa-hcp-bkp-" + mcName + "-" + clusterId}
 
-		detachIAMRolePolicyOutput, err := runCommand("bash", "-c", detachIAMRolePolicyCmd)
+		detachIAMRolePolicyOutput, err := runCommand(awsCmd, detachIAMRolePolicyListArgs...)
 		if err != nil {
-			return fmt.Errorf("Policies are empty or role does not exist: %s", err)
+			fmt.Printf("Policies are empty or role does not exist: %s", err)
 		}
 		fmt.Printf("Role policy %s is detached successfully.\n", detachIAMRolePolicyOutput)
 	}
 
 	// 2. Delete IAM Role
-	deleteRoleCmd = "aws iam delete-role --role-name " + roleName
-	deleteRoleCmdOutput, err := runCommand("bash", "-c", deleteRoleCmd)
+	var deleteRoleListArgs = []string{"iam", "delete-role", "--role-name", roleNamePrefix + mcName + "-" + clusterId}
+	deleteRoleCmdOutput, err := runCommand(awsCmd, deleteRoleListArgs...)
 	if err != nil {
-		return fmt.Errorf("failed to delete IAM role '%s': %w", roleName, err)
+		fmt.Printf("failed to delete IAM role '%s': %w", roleNamePrefix+mcName+"-"+clusterId, err)
 	}
 	fmt.Printf("Successfully deleted IAM role '%s'.\n", deleteRoleCmdOutput)
 
 	// 3. Delete S3 Bucket (and its contents first)
 	fmt.Printf("Attempting to delete S3 bucket '%s'...\n", bucketName)
-	s3Cmd := "aws s3 rb s3://" + bucketName + " --force"
+	s3CmdListArgs := []string{"s3", "rb", "s3://" + bucketName, " --force"}
 	// Execute the s3 command
-	s3CmdOutput, err := runCommand("bash", "-c", s3Cmd)
+	s3CmdOutput, err := runCommand(awsCmd, s3CmdListArgs...)
 	if err != nil {
-		return fmt.Errorf("failed to execute yq command: %s", err)
+		fmt.Printf("failed to execute yq command: %s", err)
 	}
-	// List and delete all objects in the bucket first
+	//List and delete all objects in the bucket first
 	fmt.Printf("Deleting all objects in bucket '%s'...\n", s3CmdOutput)
 
 	fmt.Printf("Successfully deleted S3 bucket '%s'.\n", bucketName)
@@ -171,11 +203,16 @@ func main() {
 	// os.Args[3] -> bucket name
 	// os.Args[4] -> clusterId
 	// os.Args[5] -> mc name
+	var clusterId = os.Args[1]
+	var mcName = os.Args[2]
 
-	fmt.Println("------Delete resources-------")
-	deleteResource("bsl", "BackupStorageLocation", os.Args[1])
-	deleteResource("schedule", "Schedule", os.Args[1])
-	deleteResource("backup", "BackupStorageLocation", os.Args[1])
-	deleteResource("secret", "Secret", os.Args[1])
-	cleanupAWSResources(os.Args[2], os.Args[3], os.Args[4], os.Args[5])
+	fmt.Println("------Delete Openshift resources-------")
+	cleanupAWSResources(clusterId, mcName)
+
+	fmt.Println("------Delete Openshift resources-------")
+	deleteResource("bsl", clusterId)
+	deleteResource("schedule", clusterId)
+	deleteResource("backup", clusterId)
+	deleteResource("secret", clusterId)
+	deleteResource("backuprepository", clusterId)
 }
